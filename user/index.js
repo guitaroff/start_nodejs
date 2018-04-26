541a4e95216f40d9f6730b7aba6efcb14d7cd5dc
andrey <ag82pishi@yandex.ru>
2018-04-26 19:15:55 +0300

    create dir 'user'

--- user/index.js
+++ user/index.js
@@ -1,12 +1,13 @@
 // exports
 // global
+var phrases = require('./ru');
 
 function User(name) {
   this.name = name;
 }
 
 User.prototype.hello = function (who) {
-  console.log("Hello, " + who.name);
+  console.log(phrases.Hello + ", " + who.name);
 };
 
 console.log("user.js is required!");
