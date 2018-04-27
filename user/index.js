e8b4e167ac504c83f65e3709ada7803a1a0e1bd7
andrey <ag82pishi@yandex.ru>
2018-04-27 19:15:57 +0300

    added simple database and logger

--- user/index.js
+++ user/index.js
@@ -1,16 +1,19 @@
 // exports
 // global
-var phrases = require('./ru');
+var db = require('../db');
+var log = require('../logger')(module);
 
 function User(name) {
   this.name = name;
 }
 
 User.prototype.hello = function (who) {
-  console.log(phrases.Hello + ", " + who.name);
+  log(db.getPhrase("Hello") + ", " + who.name);
 };
 
 console.log("user.js is required!");
 
-exports.User = User;
+module.exports = User;
 //global.User = User;
+
+//console.log(module);
